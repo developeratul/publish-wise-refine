59259c752d299bb32119444ae4974447afee4423
DevR <azammmgol@gmail.com>
2023-07-05 21:37:47 +0600

    feat: Added button to scroll to bottom

--- app/dashboard/blog/[blogId]/layout.tsx
+++ app/dashboard/blog/[blogId]/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Icon from "@/components/Icon";
 import { AppProps } from "@/types";
-import { ActionIcon, Affix, Container, Transition, rem } from "@mantine/core";
+import { ActionIcon, Affix, Container, Stack, Transition, rem } from "@mantine/core";
 import { useWindowScroll } from "@mantine/hooks";
 
 export default function BlogDetailsPageLayout(props: AppProps) {
@@ -10,17 +10,26 @@ export default function BlogDetailsPageLayout(props: AppProps) {
   return (
     <Container p={0} size="xl">
       <Affix position={{ bottom: rem(20), right: rem(20) }}>
-        <Transition transition="slide-up" mounted={scroll.y > 500}>
+        <Transition transition="fade" mounted={scroll.y > 500}>
           {(transitionStyles) => (
-            <ActionIcon
-              color="blue"
-              variant="subtle"
-              size="xl"
-              style={transitionStyles}
-              onClick={() => scrollTo({ y: 0 })}
-            >
-              <Icon name="IconArrowUp" size={24} />
-            </ActionIcon>
+            <Stack style={transitionStyles} spacing={0}>
+              <ActionIcon
+                color="blue"
+                variant="subtle"
+                size="xl"
+                onClick={() => scrollTo({ y: 0 })}
+              >
+                <Icon name="IconArrowUp" size={24} />
+              </ActionIcon>
+              <ActionIcon
+                color="blue"
+                variant="subtle"
+                size="xl"
+                onClick={() => scrollTo({ y: document.body.scrollHeight })}
+              >
+                <Icon name="IconArrowDown" size={24} />
+              </ActionIcon>
+            </Stack>
           )}
         </Transition>
       </Affix>
