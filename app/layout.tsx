3d1a97040644b4374cab5f943394e632f21e43ae
DevR <azammmgol@gmail.com>
2023-07-18 16:56:02 +0600

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { inter } from "@/fonts";
 import AppProvider from "@/providers/app";
 import { AppProps } from "@/types";
 import { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -33,6 +34,22 @@ export default function RootLayout(props: AppProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-48LSD3965M"></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Google tag (gtag.js) -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-48LSD3965M"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-48LSD3965M');
+              </script>
+            `,
+          }}
+        ></script>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
